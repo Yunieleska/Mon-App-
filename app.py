6199322c1eed799d603e3553cba871d6ac82010f
Yunieleska <vanessa95320@gmail.com>
2026-07-10 15:03:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def init_db():
     conn.commit()
     conn.close()
 
+# Appel immédiat de l'initialisation
 init_db()
 
 # --- INITIALISATION SESSION ---
@@ -38,7 +39,7 @@ if not st.session_state.logged_in:
         if "forgot_step" not in st.session_state: st.session_state.forgot_step = 0
 
         if st.session_state.forgot_step == 0:
-            # Correction effectuée ici
+            # Correction : remplacement de "Rapport" par "Connexion"
             tab1, tab2 = st.tabs(["Connexion", "Inscription"])
             with tab1:
                 user_login = st.text_input("Ton pseudo", key="login_in")
